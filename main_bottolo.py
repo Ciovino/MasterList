1e6736b87787dd4ac3095403b7aa87cb6b646174
Ciovino <tarantino.giovanbattista01@gmail.com>
2023-11-14 12:47:25 +0100

    aggiunto comando /pappagallo temporaneo

--- main_bottolo.py
+++ main_bottolo.py
@@ -5,7 +5,7 @@ from bot_wrapper import BotWrapper
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
-from comandi_bot import state_cmd, back_cmd, start_cmd
+from comandi_bot import state_cmd, back_cmd, start_cmd, pappagallo_cmd
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -37,12 +37,30 @@ async def main_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     if command == 'back':
         await back_cmd.execute(list_bot, user, update, context)
+        return
 
     if command == 'start':
         await start_cmd.execute(list_bot, user, update, context)
+        return
 
     if command == 'state':
         await state_cmd.execute(list_bot, user, update, context)
+        return
+    
+    if command == 'pappagallo':
+        await pappagallo_cmd.execute(list_bot, user, update, context)
+        return
+
+async def normal_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = list_bot.is_known_user(update.effective_user.id)
+    if user == None:
+        user = list_bot.add_user(UserInfo(update.effective_user.id, update.effective_user.full_name, []))
+    
+    if user.get_current_state() == 'pappagallo':
+        await context.bot.send_message(
+            chat_id = update.effective_chat.id,
+            text = list_bot.return_mex("echo", user, update.message)
+        )
 
 if __name__ == '__main__':
     # Crea il bot
@@ -51,6 +69,10 @@ if __name__ == '__main__':
     # Comandi
     main_command_handler = MessageHandler(filters.COMMAND, main_command)
     application.add_handler(main_command_handler)
+
+    # Messaggi normali
+    normal_message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), normal_message)
+    application.add_handler(normal_message_handler)
 
     # Avvia il bot
     application.run_polling()
